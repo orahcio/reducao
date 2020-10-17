aadde5fd929c028a80bc0a25f376035b08875e8b
Orahcio Felício de Sousa <orahcio@goldenfeynman>
2020-10-17 11:12:24 -0300

    Salva as coordenadas clicadas num arquivo xlsx.

--- app.py
+++ app.py
@@ -2,18 +2,19 @@ from bokeh.plotting import figure
 from bokeh.embed import components
 from bokeh.events import Tap
 from bokeh.models import ColumnDataSource, DataTable, TableColumn, PointDrawTool, Spinner, WheelZoomTool, RadioGroup,\
-    CustomJS, Paragraph, Button, FileInput
+    CustomJS, Paragraph, Button, Slider
 from bokeh.layouts import column, row
 
 import colorcet as cc
 
 from flask import Flask, flash, render_template, request, redirect, url_for
-from flask import send_from_directory, jsonify, make_response, jsonify
+from flask import send_from_directory, jsonify, make_response 
+import json
 from werkzeug.utils import secure_filename
 import os
 
 from astropy.io import fits
-from astropy.visualization import HistEqStretch
+from astropy.visualization import HistEqStretch, ContrastBiasStretch
 from astropy.table import Table
 from astropy.coordinates import SkyCoord, match_coordinates_3d
 
@@ -54,6 +55,11 @@ def interface():
 
 @app.route('/corfit', methods=['POST'])
 def upload_cor():
+    '''
+    Essa função ficou defasada quando decidi upar o corr junto com o fit
+    Posteriormente farei a requisição diretamente ao astrometry.net para
+    não ter mais trabalho de upar lá pra depois aqui
+    '''
 
     req = request.get_json()
     
@@ -103,22 +109,22 @@ def upload_cor():
 def upload_file():
     if request.method == 'POST':
         # check if the post request has the file part
-        if 'file' not in request.files:
-            flash('No file part')
+        if 'fits' not in request.files:
+            flash('Um dos arquivos não foi postado')
 
             return redirect(request.url)
 
-        file = request.files['file']
+        ffit = request.files['fits']
         # if user does not select file, browser also
         # submit an empty part without filename
-        if file.filename == '':
+        if ffit.filename == '':
             
-            flash('No selected file')
+            flash('Ficou faltando selecionar algum arquivo')
             return redirect(request.url)
 
-        if file and allowed_file(file.filename):
-            filename = secure_filename(file.filename)
-            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+        if ffit and allowed_file(ffit.filename):
+            filename = secure_filename(ffit.filename)
+            ffit.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
 
             return redirect(url_for('plotfits',filename=filename))
 
@@ -128,22 +134,47 @@ def upload_file():
 @app.route('/plot/<filename>')
 def plotfits(filename):
     
-    with fits.open('upfolder/'+filename) as f:
-        img = f[0].data
-    
-    stretch = HistEqStretch(img) # Histograma, melhor função para granular a imagem
-    h,w = img.shape # número de linhas e colunas da matriz da imagem
-
     # Dados que serão usados para fazer computação e visualizar os pontos
     source = ColumnDataSource(dict(
         fit=[], # quando salvar estado salvar tabela
-        ra = [],
+        ra=[],
         dec=[],
         x=[],
         y=[],
         tipo=[], # se é obj, src ou sky
         banda=[] # o filtro da imagem
+    ))
+
+    # Abrindo imagem
+    with fits.open('upfolder/'+filename) as f:
+        img = f[0].data
+    
+
+    # img = np.array([[0,  1,  2,  3],
+                    # [4,  5,  6,  7],
+                    # [8,  9, 10, 11]])
+    
+    # Abrindo coordenadas se salvas
+    try:
+        corname = 'upfolder/'+filename.strip('[.fit|.fits]')+'.xlsx'
+        cordata = pd.read_excel(corname)
+        # Dados que serão usados para fazer computação e visualizar os pontos
+        source = ColumnDataSource(dict(
+            fit=[filename for i in range(len(cordata))], # quando salvar estado salvar tabela
+            ra=['na' for i in range(len(cordata))],
+            dec=['na' for i in range(len(cordata))],
+            x=cordata['x'].tolist(),
+            y=cordata['y'].tolist(),
+            tipo=cordata['tipo'], # se é obj, src ou sky
+            banda=['undef' for i in range(len(cordata))] # o filtro da imagem
         ))
+        print('Coordenadas carregadas.')
+    except FileNotFoundError:
+        print('Não há coordenadas salvas: %s' % corname)
+
+    stretch = HistEqStretch(img) # Histograma, melhor função para granular a imagem
+    h,w = img.shape # número de linhas e colunas da matriz da imagem
+    print(h,w)
 
     # Constrói a tabaela de dados que poderá ser usada para designar as posições do objeto, estrela e céu
     tabela = DataTable(source=source,columns=[
@@ -153,16 +184,20 @@ def plotfits(filename):
     ], editable=True)
     
     # Plota a imagem do arquivo fit
-    p = figure(plot_width=w, plot_height=h,tooltips=[("x", "$x"), ("y", "$y"), ("value", "@image")],\
+    p = figure(plot_height=h, plot_width=w, tooltips=[("x", "$x"), ("y", "$y"), ("value", "@image")],\
         active_scroll='wheel_zoom')
-    p.image(image=[stretch(normal(img))], x=0, y=0, dw=w, dh=h, palette=cc.CET_CBL2, level="image")
+    nimg = stretch(normal(img)).tolist()
+    # print(nimg.tolist())
+    # print(img.tolist())
+    im = p.image(image=[nimg], x=0, y=0, dw=w, dh=h, palette='Greys256', level="image")
+    # im = p.image(image=[img], x=0, y=0, dw=w, dh=h, palette=cc.CET_CBL2, level="image")
     p.x_range.range_padding = p.y_range.range_padding = 0
     p.grid.grid_line_width = 0
 
     # Os círculos que serão inseridos
     c = p.circle('x','y', source=source, color='red', fill_color=None, radius=8, line_width=2)
     cd = p.circle_dot('x','y', source=source, color='red', size=2)
-    tool = PointDrawTool(renderers=[c,cd],empty_value='obj')
+    tool = PointDrawTool(renderers=[c,cd],empty_value='na')
     p.add_tools(tool)
     p.toolbar.active_tap = tool
 
@@ -174,75 +209,70 @@ def plotfits(filename):
     radio_title = Paragraph(text='Escolha o tipo:')
     LABELS = ['obj','src','sky']
     radio_group = RadioGroup(labels=LABELS, active=0)
-    # radio_group.js_link('labels',tool,'empty_value',attr_selector='active')
-    callback = CustomJS(args=dict(tool=tool), code='''
-           tool.empty_value = cb_obj.labels[cb_obj.active]
-           tool.change.emmit()
-    ''')
-    radio_group.js_on_change('active',callback)
 
-    # Fazer o upload do corr.fit da imagem
-    upload_cor = FileInput()
-    upload_cor.js_on_change('value',CustomJS(args = dict(source=source), code = '''
+    # Evento de mudança da tabela de dados, para inserir dados padrão nas colunas inalteradas
+    source.js_on_change('data', CustomJS(args=dict(radio=radio_group), code='''
+            var data = cb_obj.data;
 
-            var data = source.data;
-            console.log(data)
+            const n = data['x'].length;
+            const labels = radio.labels;
+            var aux = `${window.location.pathname}`;
+            aux = aux.slice(6,aux.length);
+            if(data['tipo'][n-1]=='na') {
+                data['fit'][n-1] = aux;
+                data['tipo'][n-1] = labels[radio.active];
+                data['banda'][n-1] = 'undef';
+            }
 
-            var entry = {
-                url: `${window.location.href}`,
-                ra: data['ra'],
-                dec: data['dec'],
-                x: data['x'],
-                y: data['y'],
-                tipo: data['tipo'],
-                arquivo: cb_obj.value,
-                }
-            console.log(entry)
+            cb_obj.change.emit();
+    '''))
+    # Fazer o upload do corr.fit da imagem (defasado)
 
-            fetch(`${window.origin}/corfit`, {
-                method: "POST",
-                credentials: "include",
-                body: JSON.stringify(entry),
-                cache: "no-cache",
-                headers: new Headers({
-                    "content-type": "application/json"
-                })
-            })
-            .then(function (response) {
-                if (response.status !== 200) {
-                    console.log(`Looks like there was a problem. Status code: ${response.status}`);
-                    return;
-                }
-                response.json().then(function (resp) {
-                    let aux = window.location.pathname
-                    aux = aux.slice(6,aux.length)
-                    for(var i in resp['ra']) {
-                        data['ra'].push(resp['ra'][i])
-                        data['dec'].push(resp['dec'][i])
-                        data['x'].push(resp['x'][i])
-                        data['y'].push(resp['y'][i])
-                        data['tipo'].push('src')
-                        data['banda'].push('undef')
-                        data['fit'].push(aux)
+    contrast = Slider(start=-1, end=6, value=1, step=0.05, title="Contraste")
+    contrast.js_on_change('value',CustomJS(args = dict(source=im.data_source, im=nimg), code = '''
+
+            const x = im
+            var y = source.data['image'][0];
+            // console.log(y)
+            const ni = y.length
+            const nj = y[0].length
+            const n = ni*nj
+            var c = cb_obj.value
+            // console.log(c)
+            var max = 0 
+            var min = 2**64-1
+
+            // console.log('x: ', x) 
+            // Clip values
+            const clip = (y) => {
+                for(let i=0;i<ni;i++) {
+                    for(let j=0;j<nj;j++) {
+                        if(max<y[i][j]) { max=y[i][j] }
+                        if(min>y[i][j]) { min=y[i][j] }
                     }
-
-                    console.log('Acabaou de chegar: ', data);
-                    source.change.emit();
-                });
-            })
-            .catch(function (error) {
-                console.log("Fetch error: " + error);
-            });
+                }
+                for(let i=0;i<ni;i++) {
+                    for(let j=0;j<nj;j++) {
+                        y[i][j] = (y[i][j]-min)/(max-min)
+                    }
+                }
+                return y
+            }
+            y = clip(y)
+            for(let i=0;i<ni;i++) {
+                for(let j=0;j<nj;j++) {
+                    y[i][j] = Math.pow(x[i][j]+1e-8,10**c)
+                }
+            }
+            y = clip(y)
+            source.change.emit()
     '''))
 
     # o Botão de salvar irá enviar um json para o servidor que irá ler e fazer os procedimentos posteriores
     callback_botao = CustomJS(args=dict(source=source), code='''
-            var dados = source.data;
-            var entry = {
-                tipo: dados['tipo'],
-                x: dados['x'],
-                y: dados['y'],
-            }
+
+            var entry = source.data;
+            console.log(entry)
 
             fetch(`${window.origin}/resultado`, {
                 method: "POST",
@@ -259,7 +289,7 @@ def plotfits(filename):
                     return;
                 }
                 response.json().then(function (data) {
-                    console.log('Acabaou de chegar: ',data[x]);
+                    console.log('Acabaou de chegar: ',data['x']);
                 });
             })
             .catch(function (error) {
@@ -267,10 +297,10 @@ def plotfits(filename):
             });
 
     ''')
-    salvar = Button(label='Salvar coordenadas', button_type="success")
+    salvar = Button(label='Salvar tabela', button_type="success")
     salvar.js_on_click(callback_botao)
 
-    div, script = components(row(column(spinner,radio_title,radio_group,upload_cor,salvar), column(p,tabela, sizing_mode='scale_height')))
+    div, script = components(row(column(spinner,contrast,radio_title,radio_group,salvar), column(p,tabela, sizing_mode='scale_height')))
     return render_template('plot.html', the_div=div, the_script=script)
 
 
@@ -282,15 +312,16 @@ def create_entry():
 
     req = request.get_json()
 
-    print(req)
+    out = pd.DataFrame(req)
+    out[['x','y','tipo']].to_excel('upfolder/'+out['fit'][0].strip('[.fit|.fits]')+'.xlsx',
+                                    index=False)
+
+    print(out)
 
     # res = make_response(jsonify({"message": "OK"}), 200)
     res = make_response(req, 200)
 
     return res
-
-def click():
-    print('Clicado')
 
 
 @app.route('/uploads/<filename>')
