68ca2af259d872d41720cbbb7d75666e35504f2b
orahcio <orahcio@gmail.com>
2023-02-12 16:08:17 -0300

    Ambiente FHS com poetry

--- flake.nix
+++ flake.nix
@@ -7,25 +7,33 @@
       url = "github:numtide/flake-utils";
     };
   };
-  outputs = { nixpkgs, flake-utils, ... }: flake-utils.lib.eachDefaultSystem (system:
+  outputs = { self, nixpkgs, flake-utils, ... }: flake-utils.lib.eachDefaultSystem (system:
     let
       pkgs = import nixpkgs {
         inherit system;
       };
-    in rec {
+    in {
+      packages.fhs-environment = pkgs.buildFHSUserEnv {
+        name = "myEnv";
+        targetPkgs = pkgs: with pkgs; [
+          python3
+          poetry
+        ];
+        runScript = "bash";
+      };
+
       devShell = pkgs.mkShell {
         buildInputs = with pkgs; [
-          python3
-          python3Packages.virtualenv
+          self.packages.${system}.fhs-environment
         ];
         shellHook = ''
-          export PIP_PREFIX=$(pwd)/_build/pip_packages #Dir where built packages are stored
-          export PYTHONPATH="$PIP_PREFIX/${pkgs.python3.sitePackages}:$PYTHONPATH"
-          export PATH="$PIP_PREFIX/bin:$PATH"
-          unset SOURCE_DATE_EPOCH
-          source .venv/bin/activate
+          # export PIP_PREFIX=$(pwd)/_build/pip_packages #Dir where built packages are stored
+          # export PYTHONPATH="$PIP_PREFIX/${pkgs.python3.sitePackages}:$PYTHONPATH"
+          # export PATH="$PIP_PREFIX/bin:$PATH"
+          # unset SOURCE_DATE_EPOCH
+          # source .venv/bin/activate
+          exec myEnv
         '';
       };
-    }
-  );
+    });
 }
