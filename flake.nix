8387fe4debe692c44bd83408ecd2ce3bbe0edf3c
orahcio <orahcio@gmail.com>
2023-06-29 20:15:47 -0300

    Forma possível para rodar no meu laptop por causa do astroquery quebrado

--- flake.nix
+++ flake.nix
@@ -12,6 +12,28 @@
       pkgs = import nixpkgs {
         inherit system;
       };
+      my_astroquery = pkgs.python3Packages.buildPythonPackage rec {
+        pname = "astroquery";
+        version = "0.4.7.dev7761";
+        src = pkgs.python3Packages.fetchPypi {
+          inherit pname version;
+          sha256 = "7f66e39f0d9e22c0b7bd355ae74218797d87eab9d7236f2fcb1537ee5da7ceda";
+        };
+        propagatedbuildInputs = with pkgs.python3Packages;[
+          setuptools
+        ];
+        buildInputs = with pkgs.python3Packages;[
+          astropy
+          pyvo
+          html5lib
+          beautifulsoup4
+          keyring
+          pytest-astropy
+          pytest
+          pillow
+          matplotlib
+        ];
+      };
     in {
       devShell = pkgs.mkShell rec {
         venvDir = "./.venv";
@@ -23,7 +45,6 @@
           numpy
           werkzeug
           astropy
-          astroquery
           statsmodels
           pandas
           gunicorn
@@ -35,16 +56,25 @@
           #libGLU
           #xorg.libX11
         ];
-        shellHook = ''
+        # Esse approuch permite que eu instale pacotes que estão quebrados no nixos pelo pip
+        shellHook = ''  
           # for PyTorch
           export LD_LIBRARY_PATH=${pkgs.stdenv.cc.cc.lib}/lib
-
+          
+          # Dir where built packages are stored
+          export PIP_PREFIX=$(pwd)/_build/pip_packages
+          export PYTHONPATH="$PIP_PREFIX/${pkgs.python3.sitePackages}:$PYTHONPATH"
+          export PATH="$PIP_PREFIX/bin:$PATH"
+          
           # for Numpy
           export LD_LIBRARY_PATH=${pkgs.zlib}/lib:$LD_LIBRARY_PATH
 
         #   # GL libraries (for gym environment rendering)
         #   export LD_LIBRARY_PATH=${pkgs.libGL}/lib:$LD_LIBRARY_PATH
         #   export LD_LIBRARY_PATH=${pkgs.libGLU}/lib:$LD_LIBRARY_PATH
+
+        unset SOURCE_DATE_EPOCH
+        source .venv/bin/activate
         '';
       };
     });
