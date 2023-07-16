99e9a0596c303bb702215a44c20b228b0d4870ec
orahcio <orahcio@gmail.com>
2023-07-16 12:26:40 -0300

    debug pelo gunicorn

--- app.py
+++ app.py
@@ -707,8 +707,8 @@ def reducao(dirname):
 
 def main():
     port = int(os.environ.get("PORT",5000))
-    app.run(host="0.0.0.0", port=port, threaded=True)
-    # app.run(debug=True)
+    # app.run(host="0.0.0.0", port=port)
+    app.run(host="0.0.0.0", port=port, debug=True)
 
 
 if __name__ == "__main__":
