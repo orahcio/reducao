e6cc4f89dab5e09a28f52a714a296720925205cf
orahcio <orahcio@gmail.com>
2023-07-16 17:58:19 -0300

    Sem debug

--- app.py
+++ app.py
@@ -708,8 +708,8 @@ def reducao(dirname):
 
 def main():
     port = int(os.environ.get("PORT",5000))
-    # app.run(host="0.0.0.0", port=port)
-    app.run(host="0.0.0.0", port=port, debug=True)
+    app.run(host="0.0.0.0", port=port)
+    # app.run(host="0.0.0.0", port=port, debug=True)
 
 
 if __name__ == "__main__":
