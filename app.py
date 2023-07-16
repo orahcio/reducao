0462a1896b3eaee6f09289af63fdb70f7000506a
orahcio <orahcio@gmail.com>
2023-07-16 12:18:28 -0300

    threaded no local correto

--- app.py
+++ app.py
@@ -706,8 +706,8 @@ def reducao(dirname):
 
 
 def main():
-    port = int(os.environ.get("PORT",5000), threaded=True)
-    app.run(host="0.0.0.0", port=port)
+    port = int(os.environ.get("PORT",5000))
+    app.run(host="0.0.0.0", port=port, threaded=True)
     # app.run(debug=True)
 
 
