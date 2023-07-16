ffa6d6e53fb253b83144bdbe45646b740ca9b6f3
orahcio <orahcio@gmail.com>
2023-07-16 12:12:17 -0300

    Tentativa de fazer mais dados serem enviados

--- app.py
+++ app.py
@@ -706,7 +706,7 @@ def reducao(dirname):
 
 
 def main():
-    port = int(os.environ.get("PORT",5000))
+    port = int(os.environ.get("PORT",5000), threaded=True)
     app.run(host="0.0.0.0", port=port)
     # app.run(debug=True)
 
